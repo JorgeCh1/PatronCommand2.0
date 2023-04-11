c3bdf6fe3ff9227932e28c617b1ea01052bc83ce
JorgeCh1 <jorge.chavez13731@est.uca.edu.ni>
2023-04-11 10:07:08 -0600

    Prototype

---
+++ Patrones.Command.UI3/Controllers/ComputadoraController.cs
@@ -0,0 +1,54 @@
+﻿using Patrones.Command.UI3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Patrones.Command.UI3.Controllers
+{
+    public class ComputadoraController : Controller
+    {
+        private static List<Computadora> computadoras = new List<Computadora>();
+
+        //ESTE ES UN MÉTODO DE ACCIÓN QUE DEVUELVE LA VISTA INDEX Y ASINGA LA LISTA DE COMPUTADORAS
+        // A AL PROPIERDAD VIEWBAG.COMPUTADORAS Y MOSTRARA UNA TABLA CON LAS COMPUTADORAS AGG Y CLONADAS
+        public ActionResult Index()
+        {
+            ViewBag.Computadoras = computadoras;
+            return View();
+        }
+
+
+        //ESTE ES OTRO MÉTODO DE ACCIÓN QUE MANEJA LA SOLICITUD POST CUANDO SE ENVIA EL FORMUALRIO PARA 
+        //AGG UNA NUEVA COMPUTADORA, VALIDA SI EL MODELO ES VALIDO Y DE SER ASI AGG LA COMPUTADORA A LA LISTA Y
+        //LUEGO REDIRIGE AL USUARIO A LA ACCIÓN INDEX, DE LO CONTRARIO SE DEVUELVE A LA VISTA INDEX
+        [HttpPost]
+        public ActionResult AgregarComputadora(Computadora computadora)
+        {
+            if (ModelState.IsValid)
+            {
+                computadoras.Add(computadora);
+                return RedirectToAction("Index");
+            }
+            ViewBag.Computadoras = computadoras;
+            return View("Index");
+        }
+
+
+        //ESTE ULTIMO MÉTODO DE ACCIÓN PERMITE CLONAR UNA COMPUTADORA EXISTENTE EN LA LISTA DE COMPUTADORAS UTILIZANDO EL PATRON
+        //PROTOTYPE, LA ACCIÓN RECIBE EL ID QUE DESEA CLONAR Y LA CLONA Y LA AGG A LA LISTA Y FINALMENTE TE REDIGIRE AL INDEX
+        public ActionResult ClonarComputadora(int id)
+        {
+            if (id >= 0 && id < computadoras.Count)
+            {
+                var computadoraOriginal = computadoras[id];
+                var computadoraClonada = computadoraOriginal.Clonar();
+                computadoras.Add(computadoraClonada);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Patrones.Command.UI3/Models/Computadora.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Patrones.Command.UI3.Models
+{
+    //EN ESTA INTERFFAZ GENÉRICA SE DEFINE UN ÚNICO MÉTODO CLONAR()
+    public interface IClonable<T>
+    {
+        T Clonar();
+    }
+
+    //EN LA CLASE COMPUTADORA AGREGAMOS LAS PROPIEDADES QUE REPRESENTAN UNA COMPUTADORA, ADEMAS
+    //DE IMPLEMENTAR LA INTERFAZ IClonable<Computadora>
+    public class Computadora : IClonable<Computadora>
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int MemoriaRAM { get; set; }
+        public int Almacenamiento { get; set; }
+
+
+        //ESTA CLASE TIENE DOS CONSTRUCTORES UNO SIN PARAMETROS QUE ES NECESARIO PARA EL ENLACE DE MODELOS
+        //EN ASP.NET MVC FUNCIONE Y EL CONSTRUCTOR CON PARAMETROS ES PARA CREAR UNA INSTANCIA DE LA CLASE
+        //COMPUTADORA.
+        public Computadora()
+        {
+        }
+
+        // Constructor con parámetros (si es necesario)
+        public Computadora(string marca, string modelo, int memoriaRAM, int almacenamiento)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            MemoriaRAM = memoriaRAM;
+            Almacenamiento = almacenamiento;
+        }
+        //ESTE METODO IMPLEMENTA EL METODO CLONAR DEFINIDO POR LA INTERFAZ ICLONABLE<COMPUTADORA>
+        //CREA UNA NUEVA INSTANCIA DE LA CLASE COMPUTADORA Y COPIA LAS PROPIEDADES DEL OBJETO ACTUAL AL
+        //OBJETO CLONADO SIN AFECTAR AL ORIGINAL
+        public Computadora Clonar()
+        {
+            return new Computadora(Marca, Modelo, MemoriaRAM, Almacenamiento);
+        }
+    }
+}
